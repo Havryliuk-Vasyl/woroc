25d2a72942acf2237f5998653c2c71eb930fdf16
Flamerman <vasiagavryluk20@gmail.com>
2024-04-05 11:20:02 +0300

    Changes

---
+++ Woroc/templates/footer.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+        <div class="footertop">
+            <div class="container">
+                <div class="firstsection">
+                    <a href="#" class="footerlogo"><img src="../assets/images/Woroc-Logo.png" alt="Vinyl record Woroc." style="max-width: 100px; max-height: 60px;"></a>
+                    <div class="colitems">
+                        <div class="sub-title"><b>Адреса</b></div>
+                        <div class="col-item">Україна, м. Чернівці, вул. Глибоцька 2а</div>
+                        <div class="sub-title "><b>Розробник</b></div>
+                        <div class="col-item">Гаврилюк Василь&#169</div>
+                        <div class="sub-title"><b>E-mail</b></div>
+                        <div class="col-item"><a href="#">flamermanofficial@gmail.com</a></div>
+                    </div>
+                </div>
+                <div class="centersection">                        
+                    <div class="footerlogo"><a href="#"><img src="../assets/images/Woroc-Logo.png" alt="Vinyl record Woroc." style="max-width: 100px; max-height: 60px;"></a></div>
+                    <div class="colitems">
+                        <div class="col-item"><a href="../index.php">Головна</a></div>
+                        <div class="col-item"><a href="../pages/catalog.php">Каталог</a></div>
+                        <div class="col-item"><a href="../pages/cart.php">Кошик</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footerbottom">
+        </div>
+    </footer>
---
+++ Woroc/templates/header.php
@@ -0,0 +1,19 @@
+<header>
+        <div class="logo">
+            <a href="../index.php"><img src="../assets/images/Woroc-Logo.png" alt="Vinyl record Woroc." style="max-width: 100px; max-height: 60px;"></a>
+        </div>
+        <div class="menu-toggle" id="mobile-menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="menu">
+            <div class="navigation">
+                <ul>
+                    <li><a href="../index.php">Головна</a></li>
+                    <li><a href="../pages/catalog.php">Каталог</a></li>
+                    <li><a href="../pages/cart.php">Кошик</a></li>
+                </ul>
+            </div>
+        </div>
+</header>
